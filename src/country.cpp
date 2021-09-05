140983f3d29d14f637fdd08db5185398cf1da61f
Serhii Bura <serhiibura@gmail.com>
2021-09-06 00:33:42 +0300

    src: Implement Country methods
    Signed-off-by: Serhii Bura <serhiibura@gmail.com>

--- src/country.cpp
+++ src/country.cpp
@@ -1,10 +1,15 @@
 #include "../inc/country.hpp"
 #include "../inc/common.hpp"
 
-Country::Country(int mcc, const std::string &code, const std::string &name) {
-    /* TODO: Implement */
+Country::Country(int mcc, QString code, QString name)
+    : m_mcc{mcc}, m_code{code}, m_name{name} {
+    m_imgPath = getCountryCodeImgPath(m_code);
 }
 
 Country::~Country() {
     /* TODO: Implement */
+}
+
+bool Country::operator == (const Country &country) {
+    return m_mcc == country.m_mcc && m_code == country.m_code;
 }
